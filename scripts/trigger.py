1cd4f7b86b1825553810de47f56102bf5e85fa7e
Andy McKay <andy@mckay.pub>
2020-05-15 05:29:35 -0700

    init commit

---
+++ scripts/trigger.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+import sys
+import time
+
+projects = ["python-sum"]
+
+for project in projects:
+    cmd("git", "clone", "https://github.com/compare-ci/%s.git" % project)
+    os.chdir(project)
+    filename = ".timestamp"
+    with open(filename, "w") as timestamp:
+        print("Written timestamp for %s" % filename)
+        timestamp.write(str(time.time()))
+
+    def cmd(*args):
+        print("Calling:", args)
+        res = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if res.returncode != 0:
+            print("Error occurred on calling: %s" % ' '.join(args))
+            print(res.stderr.decode("utf-8"))
+            sys.exit(res.returncode)
+        return res
+
+    cmd("git", "checkout", "-b", "trigger-builds-%s" % time.time())
+    cmd("git", "config", "user.email", "andymckay@github.com")
+    cmd("git", "config", "user.name", "Andy McKay")
+    cmd("git", "commit", "-m", "Pushes on the project", "-a")
+    cmd("gh", "pr", "create", "-t", "Trigger automatic builds", "-b", "Automatic pull request trigger")
+    print("Successfully created PR.")
